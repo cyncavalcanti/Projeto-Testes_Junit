710f39a9855c2f7b0808083c84ae40a3a0808d8f
cynthia cavalcanti <cynthia.cavalcanti@gmail.com>
2022-09-14 15:12:16 -0300

    classe DTO

---
+++ src/main/java/com/projeto/api/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.projeto.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
