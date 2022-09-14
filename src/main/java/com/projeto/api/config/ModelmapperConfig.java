729ff414755988f6dcc09c953ea6c8c23e9c9de4
cynthia cavalcanti <cynthia.cavalcanti@gmail.com>
2022-09-14 15:25:13 -0300

    disponibilizando um model mapper em forma de bean

---
+++ src/main/java/com/projeto/api/config/ModelmapperConfig.java
@@ -0,0 +1,14 @@
+package com.projeto.api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelmapperConfig {
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
