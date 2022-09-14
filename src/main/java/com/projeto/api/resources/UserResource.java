abc632f70726a56910144ded6fdb3d53c04ad36d
cynthia cavalcanti <cynthia.cavalcanti@gmail.com>
2022-09-14 13:13:22 -0300

    Interface user service findById

--- src/main/java/com/projeto/api/resources/UserResource.java
+++ src/main/java/com/projeto/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.projeto.api.resources;
 
 import com.projeto.api.domain.User;
+import com.projeto.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value ="/user")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
