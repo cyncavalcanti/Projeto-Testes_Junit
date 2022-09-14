d3449844d922bff1fd0f3fa021555d1f1b6d4270
cynthia cavalcanti <cynthia.cavalcanti@gmail.com>
2022-09-14 13:12:46 -0300

    Interface user repository

---
+++ src/main/java/com/projeto/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.api.repositories;
+
+import com.projeto.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
