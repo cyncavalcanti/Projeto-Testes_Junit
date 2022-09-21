f8a37d5149c7c8cdffadf3ca314592759a09bd52
cynthia cavalcanti <cynthia.cavalcanti@gmail.com>
2022-09-21 16:54:02 -0300

    Endpoint Create para userDTO

--- src/main/java/com/projeto/api/resources/UserResource.java
+++ src/main/java/com/projeto/api/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.projeto.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,5 +31,11 @@ public class UserResource {
         List<User> list = service.findAll();// lista todos os users
         List<UserDTO> listDto = list.stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList());// vai mapear o obj x user e transformar em user dto
         return ResponseEntity.ok().body(listDto);//retorna a lista de users dto
+    }
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj ) {
+        User newObj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/projeto/api/services/UserService.java
+++ src/main/java/com/projeto/api/services/UserService.java
@@ -1,10 +1,12 @@
 package com.projeto.api.services;
 
 import com.projeto.api.domain.User;
+import com.projeto.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     User findById(Integer id);
     List<User> findAll ();
+    User create(UserDTO obj);
 }
